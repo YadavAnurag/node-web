39a3cc46ae390327ca0edc634271a1ad05edf804
YadavAnurag <anuragyadav13481@gmail.com>
2018-05-14 13:05:24 +0530

    setup heroku port

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+
+const port = process.env.PORT || 8000;
+
 var app = express();
 
 hbs.registerPartials(__dirname+'/views/partials');
@@ -51,4 +54,6 @@ app.get('/bad', (req, res)=>{
     });
 });
 
-app.listen(8000);
+app.listen(port,()=>{
+    console.log(`running on ${port}`);
+});
